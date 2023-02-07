0dd3bfd3c2b7a27d05632f7c11c31a96abeda1c2
Juan Pablo Canseco Rios <juan.canseco.rios@gmail.com>
2023-02-07 11:30:38 -0600

    Time Utils added in BuildingBlocks/Utils/Time

---
+++ src/BuildingBlocks/Utils/Time/DateTimeProvider.cs
@@ -0,0 +1,13 @@
+﻿namespace Utils.Time
+{
+    public class DateTimeProvider : IDateTimeProvider
+    {
+        public DateTime Now() => DateTime.Now;
+
+        public DateTime NowUtc() => DateTime.UtcNow;
+
+        public DateTimeOffset NowOffset() => DateTimeOffset.Now;
+
+        public DateTimeOffset NowUtcOffset() => DateTimeOffset.UtcNow;
+    }
+}
---
+++ src/BuildingBlocks/Utils/Time/IDateTimeProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace Utils.Time
+{
+    //The purpose of this interface is to make unit testing easier in dates.
+    public interface IDateTimeProvider
+    {
+        DateTime Now();
+        DateTime NowUtc();
+        DateTimeOffset NowOffset();
+        DateTimeOffset NowUtcOffset();
+    }
+}
